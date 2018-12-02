d0dc093ebaba3a77e0d929dc73bf5fcdcb0b4704
electronicsmf <electronicsmf@aol.com>
2018-12-02 08:14:39 -0500

    My First Commit

---
+++ TheOutlet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheOutlet.Startup))]
+namespace TheOutlet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
